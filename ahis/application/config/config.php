157ea6e7e3915efef94960a7a211dfb9ec7955b7
Germain <gmirindi@gmail.com>
2013-07-19 14:46:00 +0300

    added an encryption key that will be needed when using session code, please refer to CI manual for details

--- ahis/application/config/config.php
+++ ahis/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'tqIQI7yaqDP89Xy1D6rLSHM6rgIZLqlVyCswzIb2hooYdZc5Fnnb8reAPVM4jwf';
 
 /*
 |--------------------------------------------------------------------------
